2673898f1c32fd23f4e53a193aa4910b245c0fa7
Jared Mendoza Ramirez <raul_debek@hotmail.com>
2018-02-03 15:39:09 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'Ji3oQSudnh4WMIbUjuYqtV9mw', 
+  consumer_secret:      '29k2By2LdbVpt3PHPUt9twxHR3Un8aqOZUiBuMMZ89QqGNxs4k',
+  access_token:         '959186610716897280-Fp9jREqE6wr9rPGfWWuf51EJvOiMlQl',
+  access_token_secret:  'JV7v69OPECX7Ne6NhUkS0UNClmfqVrGTrkyMDaFyUF7KK'
+}
